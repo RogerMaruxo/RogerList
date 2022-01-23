05ceb91428804989fe92bfb24461c8c87af9d3ce
Roger Maruxo SIlva <89660924+RogeMaruxo@users.noreply.github.com>
2022-01-23 03:25:55 -0300

    Projeto de Lembrete de Tarefas.

---
+++ app/src/main/java/com/cat/rogerlist/ui/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.cat.rogerlist.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.cat.rogerlist.databinding.ActivityMainBinding
+import com.cat.rogerlist.datasource.TaskDataSource
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
+        updateList()
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+        }
+
+        adapter.listenerEdit = {
+            val intent = Intent (this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            startActivityForResult(intent, CREATE_NEW_TASK)
+        }
+
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CREATE_NEW_TASK && resultCode == RESULT_OK) updateList()
+    }
+
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
+    }
+
+    companion object {
+        private const val CREATE_NEW_TASK = 1000
+    }
+}
